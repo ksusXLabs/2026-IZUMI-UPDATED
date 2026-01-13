5abe7a5c1f396448b85398ee6f74a562a5627dd5
KUSAL S D <156213745+ksusXLabs@users.noreply.github.com>
2026-01-13 06:29:34 -0800

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/9fn3ay.png",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 IZUMI-LITE Is Alive Now😍*",
+BOT_OWNER: '94752425527',  // Replace with the owner's phone number
 
 
 
