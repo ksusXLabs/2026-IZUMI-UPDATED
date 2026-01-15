759fb58524ac4fe894b10695f23f7a028780e9e1
KUSAL S D <156213745+ksusXLabs@users.noreply.github.com>
2026-01-15 01:54:16 -0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "yoQAkajJ#WlJXfboNAvFxppQxL7aaUlvuKjbjr12kZwDb7AY2bqU",
+SESSION_ID: process.env.SESSION_ID || "j1ZE2Bpa#vM9wLsjRV5QdP7G2EfTjhl2vKjUS6KbMB6X4eA3TE8U",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/9fn3ay.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 IZUMI-LITE Is Alive Now😍*",
 BOT_OWNER: '94752425527',  // Replace with the owner's phone number
