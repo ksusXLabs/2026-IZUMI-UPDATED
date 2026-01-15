f4a227b9dc38f923e289e8d16c5971b1941d7427
KUSAL S D <156213745+ksusXLabs@users.noreply.github.com>
2026-01-15 06:03:34 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,35 @@
 const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+if (fs.existsSync('config.env')) {
+    require('dotenv').config({ path: './config.env' });
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "j1ZE2Bpa#vM9wLsjRV5QdP7G2EfTjhl2vKjUS6KbMB6X4eA3TE8U",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/9fn3ay.png",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 IZUMI-LITE Is Alive Now😍*",
-BOT_OWNER: '94752425527',  // Replace with the owner's phone number
+    // SESSION
+    SESSION_ID: process.env.SESSION_ID || "j1ZE2Bpa#vM9wLsjRV5QdP7G2EfTjhl2vKjUS6KbMB6X4eA3TE8U",
 
+    // ALIVE SETTINGS
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/9fn3ay.png",
 
+    ALIVE_MSG: process.env.ALIVE_MSG || 
+`👋 𝐇𝐈, {user} 𝐈❜𝐀𝐌 𝐀𝐋𝐈𝐕𝐄 👾
 
+╭─「 ᴅᴀᴛᴇ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ 」
+│📅 Date : {date}
+│⏰ Time : {time}
+╰──────────●●►
+
+╭─「 ꜱᴛᴀᴛᴜꜱ ᴅᴇᴛᴀɪʟꜱ 」
+│👤 User : {user}
+│✒️ Prefix : !
+│🧬 Version : v1.0.0
+│🎈 Platform : Linux
+│📡 Host : {host}
+│📟 Uptime : {uptime}
+│📂 Memory : {memory}
+╰──────────●●►
+
+🛠 Developer : Dev.RabbitZz 🥕`,
+
+    // OWNER
+    BOT_OWNER: '94752425527'
 };
